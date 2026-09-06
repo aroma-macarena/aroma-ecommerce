537625a8c1be5a45c1245b3ef8df992eff85efc2
Santiago M <fsmm19@proton.me>
2026-09-06 18:05:22 -0500

    fix: remove duplicate import statements in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
+import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
-import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
